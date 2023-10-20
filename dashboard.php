a889bd1e30a6ca4c95b68d64a06c92e8e54f4513
carlos-heinrich <carlosroheinrich@gmail.com>
2023-10-20 12:52:05 -0300

    fgh

---
+++ dashboard.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["usuario"])) {
+    header("location:login.php");
+    exit;
+}
+
+echo "Bem-vindo," . $_SESSION ["usuario"] . "!Esta é a página de dashboard.";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deshboard</title>
+</head>
+<body>
+    <h1>Deshboard</h1>
+</body>
+</html>,
